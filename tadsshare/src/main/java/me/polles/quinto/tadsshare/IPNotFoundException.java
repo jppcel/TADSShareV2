f41b3b8d2a17587cce007e930638dd8b3ba70d47
João Paulo Polles <joaopaulopolles@gmail.com>
2017-04-15 18:46:42 -0300

    Exceptions usadas...

---
+++ tadsshare/src/main/java/me/polles/quinto/tadsshare/IPNotFoundException.java
@@ -0,0 +1,10 @@
+package me.polles.quinto.tadsshare;
+
+public class IPNotFoundException extends RuntimeException{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7005945332893277347L;
+	
+}
---
+++ tadsshare/src/main/java/me/polles/quinto/tadsshare/PortNotFoundException.java
@@ -0,0 +1,10 @@
+package me.polles.quinto.tadsshare;
+
+public class PortNotFoundException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2931475989638990352L;
+
+}
